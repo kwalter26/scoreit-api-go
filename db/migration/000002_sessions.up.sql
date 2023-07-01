a0382b29d3a13c5de2cbfb2c13868639b8c8a83e
Kyle Walter <walter.kl26@gmail.com>
2023-06-30 22:31:15 -0500

    Add sessions table

---
+++ db/migration/000002_sessions.up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "sessions"
+(
+    "id"            uuid PRIMARY KEY,
+    "user_id"       uuid        NOT NULL,
+    "refresh_token" varchar     NOT NULL,
+    "user_agent"    varchar     NOT NULL,
+    "client_ip"     varchar     NOT NULL,
+    "is_blocked"    boolean     NOT NULL DEFAULT false,
+    "expires_at"    timestamptz,
+    "created_at"    timestamptz NOT NULL DEFAULT (now())
+);
+
+ALTER TABLE "sessions"
+    ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
