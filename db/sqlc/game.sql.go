04d0a251c82703a3c0c4513e28f79edc93ba9be2
Kyle Walter <walter.kl26@gmail.com>
2023-07-14 22:35:43 -0500

    Updates

--- db/sqlc/game.sql.go
+++ db/sqlc/game.sql.go
@@ -12,8 +12,8 @@ import (
 )
 
 const createGame = `-- name: CreateGame :one
-INSERT INTO game (home_team_id,away_team_id,home_score,away_score)
-VALUES ($1,$2,$3,$4)
+INSERT INTO game (home_team_id, away_team_id, home_score, away_score)
+VALUES ($1, $2, $3, $4)
 RETURNING id, home_team_id, away_team_id, home_score, away_score
 `
 
