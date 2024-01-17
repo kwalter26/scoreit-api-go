6a1e0438cb6510ef969fa16d97903ca2fae8c6f3
Kyle Walter <walter.kl26@gmail.com>
2024-01-16 20:59:57 -0600

    Refactor paths in security roles test
    Extracted hard-coded paths used in security roles test to constant variables for readability and maintainability. This change improves the clarity of the test conditions and makes it easier to modify paths if required in the future
    
    SCORE-15.

--- security/roles_test.go
+++ security/roles_test.go
@@ -9,6 +9,17 @@ import (
 	"testing"
 )
 
+const (
+	validModelPath    = "../test/resources/test_authz_model.conf"
+	validPolicyPath   = "../test/resources/test_authz_policy.csv"
+	invalidModelPath  = "path/to/invalid/model"
+	invalidPolicyPath = "path/to/invalid/policy"
+	badPolicyPath     = "../test/bad_authz_policy.csv"
+	badModelPath      = "../test/bad_authz_model.conf"
+	testModelConfFile = "test_authz_model.conf"
+	testPolicyCsvFile = "test_authz_policy.csv"
+)
+
 func TestNewEnforcer(t *testing.T) {
 	tests := []struct {
 		name    string
@@ -18,53 +29,52 @@ func TestNewEnforcer(t *testing.T) {
 	}{
 		{
 			name:    "valid case",
-			config:  util.Config{CasbinModelPath: "../test/resources/test_authz_model.conf", CasbinPolicyPath: "../test/resources/test_authz_policy.csv"},
+			config:  util.Config{CasbinModelPath: validModelPath, CasbinPolicyPath: validPolicyPath},
 			fs:      test.SecurityResources(),
 			wantErr: false,
 		},
 		{
 			name:    "invalid model path",
-			config:  util.Config{CasbinModelPath: "path/to/invalid/model", CasbinPolicyPath: "../test/test_authz_policy.csv"},
+			config:  util.Config{CasbinModelPath: invalidModelPath, CasbinPolicyPath: validPolicyPath},
 			fs:      test.SecurityResources(),
 			wantErr: false,
 		},
 		{
 			name:    "invalid policy path",
-			config:  util.Config{CasbinModelPath: "../test/test_authz_model.conf", CasbinPolicyPath: "path/to/invalid/policy"},
+			config:  util.Config{CasbinModelPath: validModelPath, CasbinPolicyPath: invalidPolicyPath},
 			fs:      test.SecurityResources(),
 			wantErr: false,
 		},
 		{
 			name:    "invalid model and policy paths. uses embedded fs",
-			config:  util.Config{CasbinModelPath: "path/to/invalid/model", CasbinPolicyPath: "path/to/invalid/policy"},
+			config:  util.Config{CasbinModelPath: invalidModelPath, CasbinPolicyPath: invalidPolicyPath},
 			fs:      test.SecurityResources(),
 			wantErr: false,
 		},
 		{
 			name:    "invalid embedded fs",
-			config:  util.Config{CasbinModelPath: "path/to/invalid/model", CasbinPolicyPath: "path/to/invalid/policy"},
+			config:  util.Config{CasbinModelPath: invalidModelPath, CasbinPolicyPath: invalidPolicyPath},
 			fs:      &embed.FS{},
 			wantErr: true,
 		},
 		{
 			name:    "bad policy invalid csv",
-			config:  util.Config{CasbinModelPath: "path/to/invalid/model", CasbinPolicyPath: "../test/bad_authz_policy.csv"},
+			config:  util.Config{CasbinModelPath: invalidModelPath, CasbinPolicyPath: badPolicyPath},
 			fs:      test.SecurityResources(),
 			wantErr: true,
 		},
 		{
 			name:    "bad model embedded fs",
-			config:  util.Config{CasbinModelPath: "../test/bad_authz_model.conf", CasbinPolicyPath: "path/to/invalid/policy"},
+			config:  util.Config{CasbinModelPath: badModelPath, CasbinPolicyPath: invalidPolicyPath},
 			fs:      test.SecurityResources(),
 			wantErr: true,
 		},
 	}
 
-	// Create a file named test_authz_model.conf
-
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			fileA, errA := os.Create("test_authz_model.conf")
+			// Create a file named test_authz_model.conf
+			fileA, errA := os.Create(testModelConfFile)
 			if errA != nil {
 				t.Fatal(errA)
 			}
@@ -74,7 +84,7 @@ func TestNewEnforcer(t *testing.T) {
 			}(fileA)
 
 			// Create a file named test_authz_policy.csv
-			fileB, errB := os.Create("test_authz_policy.csv")
+			fileB, errB := os.Create(testPolicyCsvFile)
 			if errB != nil {
 				t.Fatal(errB)
 			}
@@ -88,9 +98,9 @@ func TestNewEnforcer(t *testing.T) {
 				t.Errorf("NewEnforcer() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			err = os.Remove("test_authz_model.conf")
+			err = os.Remove(testModelConfFile)
 			require.NoError(t, err)
-			err = os.Remove("test_authz_policy.csv")
+			err = os.Remove(testPolicyCsvFile)
 			require.NoError(t, err)
 		})
 	}
